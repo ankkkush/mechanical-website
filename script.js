af0685a88f242bba4e730344a9b8c4ba0752dec0
Ankush Attri <ankushattri1945@gmail.com>
2024-04-03 23:58:01 +0530

    main

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var menuIcon = document.querySelector(".menu-icon");
-    var navLinks = document.querySelector(".nav-links");
-
-    menuIcon.addEventListener("click", function () {
-        navLinks.classList.toggle("active");
-    });
-});
